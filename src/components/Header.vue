68bf3bbbda5bd94f1a10094f2a0a3c37a13d2b64
Dylan Kossoy <dylankossoy@Dylans-MacBook-Air-264.local>
2025-01-16 16:11:10 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Routerlink } from 'vue-router'
+import Logo from './Logo.vue'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <Logo />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+:slotted(*) {
+  font-size: var(--fs-600);
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <img src="myLogo.png.webp" alt="logo alt" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  max-width: 100px;
+}
+</style>
